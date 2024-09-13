7a07323f73d17789bc6b9bcff4075b822538f7ca
BenjaminCavanagh46975373 <79910849+BenC989@users.noreply.github.com>
2024-09-14 09:49:46 +1000

    Added player movement

--- COMP2160_Week08_UnityProject/Assets/Scripts/Player.cs
+++ COMP2160_Week08_UnityProject/Assets/Scripts/Player.cs
@@ -1,18 +1,44 @@
+// Include packages
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Define input action variables
+    private PlayerActions actions;
+    private InputAction movementAction;
+
+    // Define movement and position-related variables
+    [Range(1,10)] [SerializeField] private float speed = 5f;
+    private float horizontalMovement;
+    private float verticalMovement;
+
+    private void Awake()
     {
-        
+        actions = new PlayerActions();
+        movementAction = actions.movement.move;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        movementAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        movementAction.Disable();
+    }
+
+    private void Update()
+    {
+        // Read player movement input
+        horizontalMovement = movementAction.ReadValue<Vector2>().x;
+        verticalMovement = movementAction.ReadValue<Vector2>().y;
+
+        // Move the player
+        transform.Translate(Vector3.forward * speed * verticalMovement * Time.deltaTime, Space.Self);
+        transform.Translate(Vector3.right * speed * horizontalMovement * Time.deltaTime, Space.Self);
     }
 }
