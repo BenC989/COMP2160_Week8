a88fc4bd7f34b3faaa394c9187394298ee2b403c
BenC989 <79910849+BenC989@users.noreply.github.com>
2024-09-13 10:53:49 +1000

    Started scorekeeper

---
+++ COMP2160_Week08_UnityProject/Assets/Scripts/ScoreKeeper.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreKeeper : MonoBehaviour
+{
+    private int currentScore = 0;
+
+    void OnPickup()
+    {
+        /*
+        * LOOK AT THE ER DIAGRAM. IT NEEDS TO SUBSCRIBE TO THE COIN ONPICKUPEVENT
+        * FOR HOW TO SUBSCRIBE TO EVENTS, LOOK AT THE WEEK 7 PRAC, IT'S ALL THERE
+        */
+    }
+}
